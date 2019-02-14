b153075c273c5a9e783ae9448a665980f308f353
Liron Navon <liron.navon@clockwork.nl>
2019-02-14 12:56:19 +0100

    removed .idea directory

--- 5_files-and-templates/handlebars_template/index.js
+++ 5_files-and-templates/handlebars_template/index.js
@@ -7,27 +7,32 @@ const port = 3000;
 
 const viewsDirectory = path.join(__dirname, 'views');
 
+// setup the handlebars engine
 app.engine('.hbs', expressHandlebars({
     defaultLayout: 'main',
     extname: '.hbs',
     layoutsDir: path.join(viewsDirectory, 'layouts'),
 }));
 
+// setup the views directory and file extension for express to look for
 app.set('view engine', '.hbs');
 app.set('views', viewsDirectory);
 
+// render home with some generic name
 app.get('/', (request, response) => {
     response.render('home', {
         name: 'John'
     });
 });
 
+// render home with a name
 app.get('/:name', (request, response) => {
     response.render('home', {
         name: request.params.name
     });
 });
 
+// listen for calls
 app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened 😥', err)
