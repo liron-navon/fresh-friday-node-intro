50d949e8489504859386936207e26035c5e1a7f5
Liron Navon <liron.navon@clockwork.nl>
2019-02-14 13:59:11 +0100

    simplified assignment

--- 1_hello-world/assignment.js
+++ 1_hello-world/assignment.js
@@ -1,32 +1,18 @@
 /**
-    Write a script that will ask for a "colors array" separated by commas, a "number" and a "character".
-    Print lines by the number, with the given color and character:
-
-    It's OK to allow only two color options.
+    Write a script that will ask for a "number" and a "string".
+    Print lines by the number, with the given string:
 
     INPUT:
     -------
     number: 5
-    character: X
-    colors: green,red
+    string: X
 
     OUTPUT
     ------
-    1. X        (green)
-    2. XX       (red)
-    3. XXX      (green)
-    4. XXXX     (red)
-    5. XXXXX    (green)
-
-    Remember:
-    you can use the color codes from: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-    for example - 
-    Red FG code 32, escaped for terminal: \x1b[31m
+    1. X
+    2. XX
+    3. XXX
+    4. XXXX
+    5. XXXXX
  */
 
- // transforms " green,red " to ["green", "red"]
- function commaSeparatedStringToArray(commaSeperatedString){
-     const withNoSpaces = commaSeparatedString.replace(/ +/g);
-     const array = withNoSpaces.split(',');
-     return array;
- }
